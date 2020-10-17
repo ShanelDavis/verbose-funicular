64ca92326deb080d7519497580b49aadde008c5e
ShanelDavis <nshanelh@gmail.com>
2020-10-17 12:13:14 -0400

    Routes update

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const campsiteRouter = express.Router();
+const partnerRouter = express.Router();
 
-campsiteRouter.use(bodyParser.json());
+partnerRouter.use(bodyParser.json());
 
-campsiteRouter
+partnerRouter
   .route('/')
   .all((req, res, next) => {
     res.statusCode = 200;
@@ -13,43 +13,43 @@ campsiteRouter
     next();
   })
   .get((req, res) => {
-    res.end('Will send all the campsites to you');
+    res.end('Will send all the partners to you');
   })
   .post((req, res) => {
     res.end(
-      `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
+      `Will add the partner: ${req.body.name} with description: ${req.body.description}`
     );
   })
   .put((req, res) => {
     res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
+    res.end('PUT operation not supported on /partners');
   })
   .delete((req, res) => {
-    res.end('Deleting all campsites');
+    res.end('Deleting all partners');
   });
 
-campsiteRouter
-  .route('/:campsiteId')
+partnerRouter
+  .route('/:partnerId')
   .all((req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     next();
   })
   .get((req, res) => {
-    res.end('Will send all the campsites to you');
+    res.end('Will send all the partners to you');
   })
   .post((req, res) => {
     res.end(
-      `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
+      `Will add the partner: ${req.body.name} with description: ${req.body.description}`
     );
   })
   .put((req, res) => {
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite: ${req.body.name}
+    res.write(`Updating the partner: ${req.params.partnerId}\n`);
+    res.end(`Will update the partner: ${req.body.name}
         with description: ${req.body.description}`);
   })
   .delete((req, res) => {
-    res.end('Deleting all campsites');
+    res.end('Deleting all partners');
   });
 
-module.exports = campsiteRouter;
+module.exports = partnerRouter;
